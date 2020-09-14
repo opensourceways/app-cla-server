861e4bd894f3b532fb5a54ecf168889b726ed1dd
zengchen1024 <chenzeng2@huawei.com>
2020-09-14 16:41:39 +0800

    refactor individual signing

--- dbmodels/individual-signing.go
+++ dbmodels/individual-signing.go
@@ -1,6 +1,8 @@
 package dbmodels
 
 type IndividualSigningInfo struct {
-	Email string          `json:"email" required:"true"`
-	Info  TypeSigningInfo `json:"info,omitempty"`
+	Email   string          `json:"email" required:"true"`
+	Name    string          `json:"name" required:"true"`
+	Enabled bool            `json:"enabled"`
+	Info    TypeSigningInfo `json:"info,omitempty"`
 }

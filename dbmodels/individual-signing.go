06967cc96565570c37ec326f27428e6db18d7722
zengchen1024 <chenzeng2@huawei.com>
2020-09-25 14:22:38 +0800

    refactor the data model between cla api and db api

--- dbmodels/individual-signing.go
+++ dbmodels/individual-signing.go
@@ -1,16 +1,9 @@
 package dbmodels
 
 type IndividualSigningInfo struct {
-	Email   string          `json:"email" required:"true"`
-	Name    string          `json:"name" required:"true"`
+	Email   string          `json:"email"`
+	Name    string          `json:"name"`
+	Date    string          `json:"date"`
 	Enabled bool            `json:"enabled"`
-	Date    string          `json:"date" required:"true"`
-	Info    TypeSigningInfo `json:"info,omitempty"`
-}
-
-type IndividualSigningCheckInfo struct {
-	Platform string `json:"platform" required:"true"`
-	OrgID    string `json:"org_id" required:"true"`
-	RepoID   string `json:"-" required:"true"`
-	Email    string `json:"-" required:"true"`
+	Info    TypeSigningInfo `json:"info"`
 }
--- models/individual-signing.go
+++ models/individual-signing.go
@@ -5,32 +5,15 @@ import (
 	"github.com/opensourceways/app-cla-server/util"
 )
 
-type IndividualSigning struct {
-	Email   string                   `json:"email"`
-	Name    string                   `json:"name"`
-	Date    string                   `json:"date"`
-	Enabled bool                     `json:"enabled"`
-	Info    dbmodels.TypeSigningInfo `json:"info"`
-}
+type IndividualSigning dbmodels.IndividualSigningInfo
 
 func (this *IndividualSigning) Create(claOrgID string, enabled bool) error {
 	this.Date = util.Date()
 	this.Enabled = enabled
-	p := dbmodels.IndividualSigningInfo{}
-	if err := util.CopyBetweenStructs(this, &p); err != nil {
-		return err
-	}
 
-	return dbmodels.GetDB().SignAsIndividual(claOrgID, p)
+	return dbmodels.GetDB().SignAsIndividual(claOrgID, *(*dbmodels.IndividualSigningInfo)(this))
 }
 
 func IsIndividualSigned(platform, orgID, repoId, email string) (bool, error) {
-	opt := dbmodels.IndividualSigningCheckInfo{
-		Platform: platform,
-		OrgID:    orgID,
-		RepoID:   repoId,
-		Email:    email,
-	}
-
-	return dbmodels.GetDB().IsIndividualSigned(opt)
+	return dbmodels.GetDB().IsIndividualSigned(platform, orgID, repoId, email)
 }

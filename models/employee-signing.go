ce7d9be40180aeade33243f466ef48b928294324
zengchen1024 <chenzeng2@huawei.com>
2020-09-25 16:58:26 +0800

    refactor api of employee signing

--- models/employee-signing.go
+++ models/employee-signing.go
@@ -2,24 +2,6 @@ package models
 
 import "github.com/opensourceways/app-cla-server/dbmodels"
 
-type EmployeeSigning struct {
-	CLAOrgID string                   `json:"cla_org_id"`
-	Email    string                   `json:"email"`
-	Name     string                   `json:"name"`
-	Enabled  bool                     `json:"enabled"`
-	Info     dbmodels.TypeSigningInfo `json:"info,omitempty"`
-}
-
-func (this *EmployeeSigning) Create(claOrgID string) error {
-	p := dbmodels.EmployeeSigningInfo{
-		Email:   this.Email,
-		Name:    this.Name,
-		Enabled: false,
-		Info:    this.Info,
-	}
-	return dbmodels.GetDB().SignAsEmployee(claOrgID, p)
-}
-
 type EmployeeSigningListOption dbmodels.IndividualSigningListOption
 
 func (this EmployeeSigningListOption) List() (map[string][]dbmodels.IndividualSigningBasicInfo, error) {

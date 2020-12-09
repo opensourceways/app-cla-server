d3e8186ab8ead9c8f559d519902faac68847fb12
zengchen1024 <chenzeng2@huawei.com>
2020-12-09 22:40:16 +0800

    update corp pdf model

--- mongodb/models.go
+++ mongodb/models.go
@@ -3,6 +3,7 @@ package mongodb
 const (
 	fieldLinkStatus  = "link_status"
 	fieldOrgIdentity = "org_identity"
+	fieldLinkID      = "link_id"
 
 	// 'ready' means the doc is ready to record the signing data currently.
 	// 'unready' means the doc is not ready.
@@ -13,9 +14,7 @@ const (
 )
 
 type dCorpSigningPDF struct {
-	OrgIdentity string `bson:"org_identity" json:"org_identity" required:"true"`
-	LinkStatus  string `bson:"link_status" json:"link_status"`
-
+	LinkID string `bson:"link_id" json:"link_id" required:"true"`
 	CorpID string `bson:"corp_id" json:"corp_id" required:"true"`
 	PDF    []byte `bson:"pdf" json:"pdf,omitempty"`
 }

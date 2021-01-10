0d53fd09d7cacf5f37e16a59b6dffffeffcca92a
zengchen1024 <chenzeng2@huawei.com>
2021-01-10 15:50:12 +0800

    update getting org email

--- controllers/error.go
+++ controllers/error.go
@@ -3,32 +3,33 @@ package controllers
 import "github.com/opensourceways/app-cla-server/models"
 
 const (
-	errSystemError             = "system_error"
-	errMissingToken            = "missing_token"
-	errUnknownToken            = "unknown_token"
-	errInvalidToken            = "invalid_token"
-	errMissingParameter        = "missing_parameter"
-	errReadingFile             = "error_reading_file"
-	errParsingApiBody          = "error_parsing_api_body"
-	errResigned                = "resigned"
-	errUnsigned                = string(models.ErrUnsigned)
-	errNoLink                  = string(models.ErrNoLink)
-	errNoEmployeeManager       = "no_employee_manager"
-	errWrongIDOrPassword       = "wrong_id_or_pw"
-	errCorpManagerExists       = string(models.ErrCorpManagerExists)
-	errNoRefreshToken          = "no_refresh_token"
-	errUnknownEmailPlatform    = "unknown_email_platform"
-	errFileNotExists           = "file_not_exists"
-	errLinkExists              = string(models.ErrLinkExists)
-	errUnknownLink             = "unknown_link"
-	errUnmatchedCLAType        = "unmatched_cla_type"
-	errCLAExists               = "cla_exists"
-	errCLAIsUsed               = "cla_is_used"
-	errAuthFailed              = "auth_failed"
-	errUnsupportedCodePlatform = "unsupported_code_platform"
-	errEmailIsUnauthorized     = "email_is_unauthorized"
-	errUnkownPurposeForAuth    = "unkown_purpose_for_auth"
-	errNotYoursOrg             = "not_yours_org"
+	errSystemError              = "system_error"
+	errMissingToken             = "missing_token"
+	errUnknownToken             = "unknown_token"
+	errInvalidToken             = "invalid_token"
+	errMissingParameter         = "missing_parameter"
+	errReadingFile              = "error_reading_file"
+	errParsingApiBody           = "error_parsing_api_body"
+	errResigned                 = "resigned"
+	errUnsigned                 = string(models.ErrUnsigned)
+	errNoLink                   = string(models.ErrNoLink)
+	errNoEmployeeManager        = "no_employee_manager"
+	errWrongIDOrPassword        = "wrong_id_or_pw"
+	errCorpManagerExists        = string(models.ErrCorpManagerExists)
+	errNoRefreshToken           = "no_refresh_token"
+	errUnknownEmailPlatform     = "unknown_email_platform"
+	errFileNotExists            = "file_not_exists"
+	errLinkExists               = string(models.ErrLinkExists)
+	errUnknownLink              = "unknown_link"
+	errUnmatchedCLAType         = "unmatched_cla_type"
+	errCLAExists                = "cla_exists"
+	errCLAIsUsed                = "cla_is_used"
+	errAuthFailed               = "auth_failed"
+	errUnsupportedCodePlatform  = "unsupported_code_platform"
+	errUnsupportedEmailPlatform = "unsupported_email_platform"
+	errEmailIsUnauthorized      = "email_is_unauthorized"
+	errUnkownPurposeForAuth     = "unkown_purpose_for_auth"
+	errNotYoursOrg              = "not_yours_org"
 )
 
 func parseModelError(err models.IModelError) *failedApiResult {

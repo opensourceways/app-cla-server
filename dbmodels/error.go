dd506203d4d85c1d6ce2cb4c17b9113426a908b4
xwzQmxx <1499273991@qq.com>
2021-07-09 11:47:44 +0800

    store pdf files on obs

--- dbmodels/error.go
+++ dbmodels/error.go
@@ -14,3 +14,27 @@ const (
 	ErrRecordExists      DBErrCode = "db_record_exists"
 	ErrMarshalDataFaield DBErrCode = "failed_to_marshal_data"
 )
+
+type dbError struct {
+	code DBErrCode
+	err  error
+}
+
+func (e dbError) Error() string {
+	if e.err == nil {
+		return ""
+	}
+	return e.err.Error()
+}
+
+func (e dbError) IsErrorOf(code DBErrCode) bool {
+	return e.code == code
+}
+
+func (e dbError) ErrCode() DBErrCode {
+	return e.code
+}
+
+func NewDBError(code DBErrCode, err error) IDBError {
+	return dbError{code: code, err: err}
+}

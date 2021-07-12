12b051fa330a14392ecb5c2e792791dd010023ba
xwzQmxx <1499273991@qq.com>
2021-07-12 17:00:34 +0800

    add client ip to build token

--- controllers/access-controller.go
+++ controllers/access-controller.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/huaweicloud/golangsdk"
 
+	"github.com/opensourceways/app-cla-server/config"
 	"github.com/opensourceways/app-cla-server/util"
 )
 
@@ -18,6 +20,7 @@ const (
 )
 
 type accessController struct {
+	RemoteAddr string      `json:"remote_addr"`
 	Expiry     int64       `json:"expiry"`
 	Permission string      `json:"permission"`
 	Payload    interface{} `json:"payload"`
@@ -26,13 +29,18 @@ type accessController struct {
 func (this *accessController) newToken(secret string) (string, error) {
 	body, err := golangsdk.BuildRequestBody(this, "")
 	if err != nil {
-		return "", fmt.Errorf("Failed to create token: build body failed: %s", err.Error())
+		return "", fmt.Errorf("failed to create token: build body failed: %s", err.Error())
 	}
 
 	token := jwt.New(jwt.SigningMethodHS256)
 	token.Claims = jwt.MapClaims(body)
 
-	return token.SignedString([]byte(secret))
+	s, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+
+	return this.encryptToken(s)
 }
 
 func (this *accessController) refreshToken(expiry int64, secret string) (string, error) {
@@ -41,9 +49,14 @@ func (this *accessController) refreshToken(expiry int64, secret string) (string,
 }
 
 func (this *accessController) parseToken(token, secret string) error {
-	t, err := jwt.Parse(token, func(t1 *jwt.Token) (interface{}, error) {
+	token1, err := this.decryptToken(token)
+	if err != nil {
+		return err
+	}
+
+	t, err := jwt.Parse(token1, func(t1 *jwt.Token) (interface{}, error) {
 		if _, ok := t1.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method")
+			return nil, fmt.Errorf("unexpected signing method")
 		}
 
 		return []byte(secret), nil
@@ -52,12 +65,12 @@ func (this *accessController) parseToken(token, secret string) error {
 		return err
 	}
 	if !t.Valid {
-		return fmt.Errorf("Not a valid token")
+		return fmt.Errorf("not a valid token")
 	}
 
 	claims, ok := t.Claims.(jwt.MapClaims)
 	if !ok {
-		return fmt.Errorf("Not valid claims")
+		return fmt.Errorf("not valid claims")
 	}
 
 	d, err := json.Marshal(claims)
@@ -72,12 +85,46 @@ func (this *accessController) isTokenExpired() bool {
 	return this.Expiry < util.Now()
 }
 
-func (this *accessController) verify(permission []string) error {
+func (this *accessController) verify(permission []string, addr string) error {
+	bingo := false
 	for _, item := range permission {
 		if this.Permission == item {
-			return nil
+			bingo = true
+			break
 		}
 	}
+	if !bingo {
+		return fmt.Errorf("not allowed permission")
+	}
 
-	return fmt.Errorf("Not allowed permission")
+	if this.RemoteAddr != addr {
+		return fmt.Errorf("unmatched remote address")
+	}
+	return nil
+}
+
+func (this *accessController) symmetricEncryptionKey() []byte {
+	return []byte(config.AppConfig.SymmetricEncryptionKey)
+}
+
+func (this *accessController) encryptToken(token string) (string, error) {
+	t, err := util.Encrypt([]byte(token), this.symmetricEncryptionKey())
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(t), nil
+}
+
+func (this *accessController) decryptToken(token string) (string, error) {
+	dst, err := hex.DecodeString(token)
+	if err != nil {
+		return "", err
+	}
+
+	s, err := util.Decrypt(dst, this.symmetricEncryptionKey())
+	if err != nil {
+		return "", err
+	}
+
+	return string(s), nil
 }

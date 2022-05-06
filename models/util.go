d8310fa6aed443852faab3bf1b575613f886f81f
liuqi <469227928@qq.com>
2022-05-06 15:39:34 +0800

    add `+` in regExpression for email

--- models/util.go
+++ models/util.go
@@ -8,7 +8,7 @@ import (
 )
 
 func checkEmailFormat(email string) IModelError {
-	rg := regexp.MustCompile("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,6}$")
+	rg := regexp.MustCompile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,6}$")
 	if !rg.MatchString(email) {
 		return newModelError(ErrNotAnEmail, fmt.Errorf("invalid email:%s", email))
 	}

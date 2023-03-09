030070197cdd3cedab1d125c04e245962d37d0ee
yangwei <348134071@qq.com>
2023-03-09 16:45:18 +0800

    verify code limit (#162)
    * verify code limit
    
    * optimize

--- routers/router.go
+++ routers/router.go
@@ -8,12 +8,20 @@
 package routers
 
 import (
+	"strings"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"github.com/astaxie/beego/logs"
+	"github.com/ulule/limiter/v3"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
 
 	"github.com/opensourceways/app-cla-server/controllers"
 )
 
 func init() {
+	runRate()
+
 	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/cla",
 			beego.NSInclude(
@@ -92,4 +100,56 @@ func init() {
 		),
 	)
 	beego.AddNamespace(ns)
+}
+
+type resp struct {
+	Data errResp `json:"data"`
+}
+
+type errResp struct {
+	ErrCode string `json:"error_code"`
+	ErrMsg  string `json:"error_message"`
+}
+
+func runRate() {
+	requestMaxRate := beego.AppConfig.String("requestLimit")
+	requestRate, _ := limiter.NewRateFromFormatted(requestMaxRate)
+	l := limiter.New(memory.NewStore(), requestRate)
+
+	beego.InsertFilter("/*", beego.BeforeRouter, func(ctx *context.Context) {
+		rateLimit(l, ctx)
+	}, true)
+}
+
+func rateLimit(l *limiter.Limiter, ctx *context.Context) {
+	opt := limiter.Options{
+		IPv4Mask:           limiter.DefaultIPv4Mask,
+		IPv6Mask:           limiter.DefaultIPv6Mask,
+		TrustForwardHeader: false,
+	}
+
+	ip := limiter.GetIP(ctx.Request, opt)
+	if strings.HasPrefix(ctx.Input.URL(), "/v1/verification-code") {
+		limiterCtx, err := l.Get(ctx.Request.Context(), ip.String())
+		if err != nil {
+			logs.Error("limiter ctx failed: %s", err.Error())
+			return
+		}
+
+		if limiterCtx.Reached {
+			logs.Info("Too Many Requests from %s on %s", ip, ctx.Input.URL())
+
+			data := resp{
+				Data: errResp{
+					ErrCode: "system_error",
+					ErrMsg:  "Too Many Requests",
+				},
+			}
+
+			ctx.Output.JSON(data, false, false)
+
+			return
+		}
+	}
+
 }

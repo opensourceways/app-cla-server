71fe6b614a7d54361532cb40bcfb4734d977d71d
yangwei999 <348134071@qq.com>
2023-03-09 14:59:37 +0800

    verify code limit

--- go.mod
+++ go.mod
@@ -12,11 +12,12 @@ require (
 	github.com/huaweicloud/golangsdk v0.0.0-20201228013212-d10065a3dc7f
 	github.com/opensourceways/community-robot-lib v0.0.0-20211110061130-6d603704302b
 	github.com/opensourceways/gofpdf v1.16.4
+	github.com/ulule/limiter/v3 v3.11.1
 	go.mongodb.org/mongo-driver v1.4.4
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
 	google.golang.org/api v0.36.0
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	k8s.io/apimachinery v0.22.1
 	sigs.k8s.io/yaml v1.2.0
 )
--- routers/router.go
+++ routers/router.go
@@ -8,12 +8,20 @@
 package routers
 
 import (
+	"strings"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"github.com/astaxie/beego/logs"
+	"github.com/ulule/limiter/v3"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
 
 	"github.com/opensourceways/app-cla-server/controllers"
 )
 
 func init() {
+	runRate()
+
 	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/cla",
 			beego.NSInclude(
@@ -92,4 +100,64 @@ func init() {
 		),
 	)
 	beego.AddNamespace(ns)
+}
+
+type resp struct {
+	Data errResp `json:"data"`
+}
+
+type errResp struct {
+	ErrCode string `json:"error_code"`
+	ErrMsg  string `json:"error_message"`
+}
+type rateLimiter struct {
+	limiter *limiter.Limiter
+}
+
+func runRate() {
+	theRateLimit := &rateLimiter{}
+	requestMaxRate := beego.AppConfig.String("requestLimit")
+	requestRate, _ := limiter.NewRateFromFormatted(requestMaxRate)
+	theRateLimit.limiter = limiter.New(memory.NewStore(), requestRate)
+	beego.InsertFilter("/*", beego.BeforeRouter, func(ctx *context.Context) {
+		rateLimit(theRateLimit, ctx)
+	}, true)
+}
+
+func rateLimit(rateLimit *rateLimiter, ctx *context.Context) {
+	var (
+		limiterCtx limiter.Context
+		err        error
+		req        = ctx.Request
+	)
+
+	opt := limiter.Options{
+		IPv4Mask:           limiter.DefaultIPv4Mask,
+		IPv6Mask:           limiter.DefaultIPv6Mask,
+		TrustForwardHeader: false,
+	}
+
+	ip := limiter.GetIP(req, opt)
+	if strings.HasPrefix(ctx.Input.URL(), "/v1/verification-code") {
+		limiterCtx, err = rateLimit.limiter.Get(req.Context(), ip.String())
+	}
+
+	if err != nil {
+		logs.Error("limiter ctx failed: %s", err.Error())
+		return
+	}
+
+	if limiterCtx.Reached {
+		logs.Info("Too Many Requests from %s on %s", ip, ctx.Input.URL())
+		data := resp{
+			Data: errResp{
+				ErrCode: "system_error",
+				ErrMsg:  "Too Many Requests",
+			},
+		}
+
+		ctx.Output.JSON(data, false, false)
+
+		return
+	}
 }

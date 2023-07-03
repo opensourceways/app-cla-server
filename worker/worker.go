c67a70893de43be6b2a35b13c31cf9ee89341cb9
zengchen1024 <chenzeng2@huawei.com>
2023-07-03 11:50:42 +0800

    refactor worker

--- worker/worker.go
+++ worker/worker.go
@@ -2,8 +2,6 @@ package worker
 
 import (
 	"fmt"
-	"os"
-	"strings"
 	"sync"
 	"time"
 
@@ -12,16 +10,19 @@ import (
 	"github.com/opensourceways/app-cla-server/models"
 	"github.com/opensourceways/app-cla-server/pdf"
 	"github.com/opensourceways/app-cla-server/signing/domain/emailservice"
-	"github.com/opensourceways/app-cla-server/signing/infrastructure/emailtmpl"
-	"github.com/opensourceways/app-cla-server/util"
 )
 
-var worker IEmailWorker
+var (
+	worker       IEmailWorker
+	pdfGenerator pdf.IPDFGenerator
+)
 
 type EmailMessage = emailservice.EmailMessage
 
 type IEmailWorker interface {
-	GenCLAPDFForCorporationAndSendIt(string, string, models.OrgInfo, models.CorporationSigning, []models.CLAField)
+	GenCLAPDFForCorporationAndSendIt(
+		string, string, *models.OrgInfo, *models.CorporationSigning, []models.CLAField,
+	)
 	SendSimpleMessage(platform string, msg *EmailMessage)
 	Shutdown()
 }
@@ -31,9 +32,10 @@ func GetEmailWorker() IEmailWorker {
 }
 
 func Init(g pdf.IPDFGenerator) {
+	pdfGenerator = g
+
 	worker = &emailWorker{
-		pdfGenerator: g,
-		stop:         make(chan struct{}),
+		stop: make(chan struct{}),
 	}
 }
 
@@ -44,9 +46,8 @@ func Exit() {
 }
 
 type emailWorker struct {
-	pdfGenerator pdf.IPDFGenerator
-	wg           sync.WaitGroup
-	stop         chan struct{}
+	wg   sync.WaitGroup
+	stop chan struct{}
 }
 
 func (w *emailWorker) Shutdown() {
@@ -59,98 +60,32 @@ func (w *emailWorker) Shutdown() {
 }
 
 func (w *emailWorker) GenCLAPDFForCorporationAndSendIt(
-	linkID, claFile string, orgInfo models.OrgInfo,
-	signing models.CorporationSigning,
+	linkID string,
+	claFile string,
+	orgInfo *models.OrgInfo,
+	signing *models.CorporationSigning,
 	claFields []models.CLAField,
 ) {
-	f := func() {
-		defer func() {
-			w.wg.Done()
-		}()
-
-		data := emailtmpl.CorporationSigning{
-			Org:         orgInfo.OrgAlias,
-			Date:        signing.Date,
-			AdminName:   signing.AdminName,
-			ProjectURL:  orgInfo.ProjectURL(),
-			SigningInfo: buildCorpSigningInfo(&signing, claFields),
-		}
-
-		file := ""
-		fileExist := func() bool {
-			return file != "" && !util.IsFileNotExist(file)
-		}
-
-		defer func() {
-			if fileExist() {
-				os.Remove(file)
-			}
-		}()
-
-		var err error
-
-		genFile := func() error {
-			if fileExist() {
-				return nil
-			}
-
-			file, err = w.pdfGenerator.GenPDFForCorporationSigning(linkID, claFile, &signing, claFields)
-			if err != nil {
-				return fmt.Errorf("error to generate pdf, err: %s", err.Error())
-			}
-
-			return nil
-		}
-
-		var msg *EmailMessage
-		genMsg := func() error {
-			if msg != nil {
-				return nil
-			}
-
-			if msg, err = data.GenEmailMsg(); err != nil {
-				return fmt.Errorf("error to gen email msg, err:%s", err.Error())
-			}
-
-			msg.Subject = fmt.Sprintf("Signing Corporation CLA on project of \"%s\"", data.Org)
-			msg.To = []string{signing.AdminEmail}
-
-			return nil
-		}
-
-		action := func() error {
-			if err := genMsg(); err != nil {
-				return err
-			}
-
-			if err := genFile(); err != nil {
-				return err
-			}
-
-			msg.Attachment = file
-			msg.From = orgInfo.OrgEmail
-			if err := emailservice.SendEmail(orgInfo.OrgEmailPlatform, msg); err != nil {
-				return fmt.Errorf("error to send email, err:%s", err.Error())
-			}
-			return nil
-		}
-
-		index := fmt.Sprintf(
-			"sending email to %s/%s/%s:%s.",
-			orgInfo.Platform, orgInfo.OrgID, orgInfo.RepoID,
-			util.EmailSuffix(signing.AdminEmail),
-		)
-
+	f := func(impl *corpPDFEmail) {
 		w.tryToSendEmail(func() error {
-			if err := action(); err != nil {
-				return fmt.Errorf("%s %s", index, err.Error())
+			err := impl.do()
+			if err != nil {
+				err = fmt.Errorf(
+					"send corp pdf of link:%s, %s", impl.linkID, err.Error(),
+				)
 			}
-			return nil
+
+			return err
 		})
+
+		impl.clean()
+
+		w.wg.Done()
 	}
 
 	w.wg.Add(1)
-	go f()
+
+	go f(newCorpPDFEmail(linkID, claFile, orgInfo, signing, claFields))
 }
 
 func (w *emailWorker) SendSimpleMessage(emailPlatform string, msg *EmailMessage) {
@@ -199,16 +134,4 @@ func (w *emailWorker) tryToSendEmail(action func() error) {
 		case <-t.C:
 		}
 	}
-}
-
-func buildCorpSigningInfo(signing *models.CorporationSigning, claFields []models.CLAField) string {
-	orders, titles := pdf.BuildCorpContact(claFields)
-
-	v := make([]string, 0, len(orders))
-	for _, i := range orders {
-		v = append(v, fmt.Sprintf("%s: %s", titles[i], signing.Info[i]))
-	}
-	v = append(v, fmt.Sprintf("Date: %s", signing.Date))
-
-	return "  " + strings.Join(v, "\n  ")
 }

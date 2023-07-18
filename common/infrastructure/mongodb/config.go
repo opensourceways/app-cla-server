dc8b1e9753d2a451b986f936c37a08e15a96b828
zengchen1024 <chenzeng2@huawei.com>
2023-07-18 20:46:54 +0800

    connect mongodb with ssl enabled

--- common/infrastructure/mongodb/config.go
+++ common/infrastructure/mongodb/config.go
@@ -1,10 +1,15 @@
 package mongodb
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Config struct {
 	Conn    string `json:"conn"       required:"true"`
 	DBName  string `json:"db"         required:"true"`
+	CAFile  string `json:"ca_file"    required:"true"`
 	Timeout int64  `json:"timeout"`
 }
 
@@ -12,6 +17,14 @@ func (cfg *Config) SetDefault() {
 	if cfg.Timeout <= 0 {
 		cfg.Timeout = 10
 	}
+}
+
+func (cfg *Config) Validate() error {
+	if !strings.Contains(cfg.Conn, "ssl=true") {
+		return errors.New("invalid mongodb conn")
+	}
+
+	return nil
 }
 
 func (cfg *Config) timeout() time.Duration {

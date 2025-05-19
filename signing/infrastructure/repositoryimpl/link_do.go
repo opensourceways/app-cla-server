4e3d1a27e6560137ebc729fbb3293009789326e7
yangwei999 <348134071@qq.com>
2025-05-19 20:00:19 +0800

    cache link

--- signing/infrastructure/repositoryimpl/link_do.go
+++ signing/infrastructure/repositoryimpl/link_do.go
@@ -8,13 +8,14 @@ import (
 )
 
 const (
-	fieldOrg       = "org"
-	fieldCLAs      = "clas"
-	fieldCLANum    = "cla_num"
-	fieldRemoved   = "removed"
-	fieldOrgAlias  = "org_alias"
-	fieldPlatform  = "platform"
-	fieldSubmitter = "submitter"
+	fieldOrg        = "org"
+	fieldCLAs       = "clas"
+	fieldCLASFields = "clas.fields"
+	fieldCLANum     = "cla_num"
+	fieldRemoved    = "removed"
+	fieldOrgAlias   = "org_alias"
+	fieldPlatform   = "platform"
+	fieldSubmitter  = "submitter"
 )
 
 func toLinkDO(v *domain.Link) linkDO {

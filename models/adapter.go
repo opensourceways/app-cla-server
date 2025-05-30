5f1bab8aabd350ab878c2d03ab6da69347a20fea
yangwei999 <348134071@qq.com>
2025-05-30 14:49:31 +0800

    api api to download previous cla pdf (#387)
    * api api to download previous cla pdf
    
    * some rename
    
    * optimize
    
    * fix bug

--- models/adapter.go
+++ models/adapter.go
@@ -152,6 +152,10 @@ func AgreeIndividualCLA(linkId string, opt *IndividualSigning) IModelError {
 	return individualSigningAdapterInstance.Agree(linkId, opt)
 }
 
+func FindIndividualSignedCLAInfo(linkId, email string) (CLAInfo, IModelError) {
+	return individualSigningAdapterInstance.FindSignedCLAInfo(linkId, email)
+}
+
 func CheckSigning(linkId string, email string) (IndividualSigned, IModelError) {
 	return individualSigningAdapterInstance.Check(linkId, email)
 }

25229ffbe5888c6a205d69122f04f800b35a2a9a
yangwei999 <348134071@qq.com>
2025-06-27 10:26:29 +0800

    ignore remote ip

--- controllers/access_controller.go
+++ controllers/access_controller.go
@@ -37,9 +37,9 @@ func (ctl *accessController) getUser() string {
 }
 
 func (ctl *accessController) verify(permission []string, addr string) error {
-	if ctl.RemoteAddr != addr {
-		return errors.New("unmatched remote address")
-	}
+	//if ctl.RemoteAddr != addr {
+	//	return errors.New("unmatched remote address")
+	//}
 
 	for _, p := range permission {
 		if p == ctl.Permission {

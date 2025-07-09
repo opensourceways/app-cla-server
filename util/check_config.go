8ee5953e7967e872bd79c9b64f4db150e42328f6
zengchen <chenzeng2@huawei.com>
2025-07-09 10:51:51 +0800

    optimize config (#397)
    * optimize config
    
    * fix bug
    
    * minor update

--- util/check_config.go
+++ util/check_config.go
@@ -123,11 +123,13 @@ func isZero(v reflect.Value) bool {
 		return v.IsNil()
 
 	case reflect.Array:
-		z := true
 		for i := 0; i < v.Len(); i++ {
-			z = z && isZero(v.Index(i))
+			if isZero(v.Index(i)) {
+				return true
+			}
 		}
-		return z
+
+		return false
 
 	case reflect.Struct:
 		var t time.Time
@@ -136,11 +138,14 @@ func isZero(v reflect.Value) bool {
 
 			return ok && value.IsZero()
 		}
-		z := true
+
 		for i := 0; i < v.NumField(); i++ {
-			z = z && isZero(v.Field(i))
+			if isZero(v.Field(i)) {
+				return true
+			}
 		}
-		return z
+
+		return false
 
 	default:
 		// Compare other types directly:
